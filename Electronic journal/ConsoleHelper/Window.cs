4d9a9fb1fcfd24b2980f9c14aa9086acf192bfa7
Zagadochnik37 <Elephant-5@yandex.ru>
2020-10-31 19:57:32 +0300

    Добавьте файлы проекта.

---
+++ Electronic journal/ConsoleHelper/Window.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Electronic_journal
+{
+    public class Window
+    {
+        public enum WindowMode
+        {
+            FullScreen = 0,
+            Fixed = 1
+        }
+        public enum WindowAlign
+        {
+            Center = 0,
+            TopLeft = 1,
+            Top = 2,
+            TopRight = 3,
+            Right = 4,
+            BottomRight = 5,
+            Bottom = 6,
+            BottomLeft = 7,
+            Left = 8
+        }
+        public WindowMode Mode;
+        public WindowAlign Align;
+
+        public bool Visible;
+
+
+    }
+}
---
+++ Electronic journal/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Electronic_journal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            WindowManager wm = new WindowManager();
+        }
+    }
+}
